a652f55f4ff27a6df80ee130bb42ac180f86c1ae
Dadson Mweya <mweyadadson@gmail.com>
2022-12-19 10:21:30 -0800

    functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,41 @@
-#include <stdio.h>
+#include <unistd.h>
+
+#include "main.h"
 
 
+
+/**
+
+   * main - Check description
+
+    * Description: It prints the word _putchar, followed by a new line.
+
+     * Return: 0.
+
+      */
 
 int main(void)
 
 {
 
-	    _putchar('\n');
+		char word[8] = "_putchar";
 
-	        return 0;
+			int i;
+
+
+
+				for (i = 0; i < 8; i++)
+
+						{
+
+									_putchar(word[i]);
+
+										}
+
+					_putchar('\n');
+
+
+
+						return (0);
 
 }
