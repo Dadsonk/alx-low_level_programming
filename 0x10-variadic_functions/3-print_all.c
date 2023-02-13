89d1ddf3446719c48caeab60a70c5d62c50670bf
Dadson Mweya <mweyadadson@gmail.com>
2023-02-13 18:45:13 +0300

    variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,91 +1,48 @@
-#include <stdarg.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
 /**
- * print_c - prints char
- * @a: list to give
- * Return: always 0
- */
-int print_c(va_list a)
-{
-	printf("%c", va_arg(a, int));
-	return (0);
-}
-/**
- * print_i - prints int
- * @a: list to give
- * Return: always 0
- */
-int print_i(va_list a)
-{
-	printf("%d", va_arg(a, int));
-	return (0);
-}
-/**
- * print_f - prints float
- * @a: list to give
- * Return: always 0
- */
-int print_f(va_list a)
-{
-	printf("%f", va_arg(a, double));
-	return (0);
-}
-/**
- * print_s - prints string
- * @a: list to give
- * Return: always 0
- */
-int print_s(va_list a)
-{
-	char *s;
-
-	s = va_arg(a, char *);
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return (0);
-	}
-	printf("%s", s);
-	return (0);
-}
-/**
- * print_all - prints all
- * @format: format string that says arg types
- *
- */
+*print_all - prints any type of character
+*@format:format
+*Return:void
+*/
 void print_all(const char * const format, ...)
 {
-	int i, j;
-	char *sep = "";
-	char *sep2 = ", ";
-	va_list anyArgs;
-	printer ops[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"s", print_s},
-		{"f", print_f},
-		{NULL, NULL}
-	};
-
-	va_start(anyArgs, format);
-	i = 0;
-	while (format != NULL && format[i])
-	{
-		j = 0;
-		while (ops[j].f != NULL)
-		{
-			if (format[i] == *(ops[j].c))
-			{
-				printf("%s", sep);
-				ops[j].f(anyArgs);
-			}
-			j++;
-		}
-		sep = sep2;
-		i++;
-	}
-	printf("\n");
-	va_end(anyArgs);
+int i = 0;
+char *str, *sprtr = "";
+va_list va;
+va_start(va, format);
+if (format)
+{
+while (format[i] != '\0')
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", sprtr, va_arg(va, int));
+break;
+case 'i':
+printf("%s%d", sprtr, va_arg(va, int));
+break;
+case 'f':
+printf("%s%f", sprtr, va_arg(va, double));
+break;
+case 's':
+str = va_arg(va, char *);
+if (str == NULL)
+{
+str = "(nil)";
+}
+printf("%s%s", sprtr, str);
+break;
+default:
+i++;
+continue;
+}
+sprtr = ", ";
+i++;
+}
+}
+printf("\n");
+va_end(va);
 }
